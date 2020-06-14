a5af870acbc71548a8bf4f3c74fb918dd3bb846a
IhabZaidi <ihab.bibou@gmail.com>
2020-06-14 17:50:05 +0100

    first commit

---
+++ Crack me/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Remove this msg")
+    End Sub
+End Class
